1decf8cf1af94b18a216a7f23f29b2c55bc1b232
stefan hranek <hranekstefan@gmail.com>
2020-05-01 17:03:18 -1000

    Register form layout set with input & onChange

--- src/routes/Register.js
+++ src/routes/Register.js
@@ -1,18 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Input, Container, Header } from "semantic-ui-react";
 
 export default class Register extends Component {
+  state = {
+    username: "",
+    email: "",
+    password: "",
+  };
 
-    state = {
-        username: '',
-        email: '',
-        password: '',
-    }
+  onChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
 
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    const { username, email, password } = this.state;
+
+    return (
+      <Container text>
+        <Header as="h2">Register</Header>
+        <Input
+          name="username"
+          onChange={this.onChange}
+          value={username}
+          placeholder="Username"
+          fluid
+        />
+        <Input
+          name="email"
+          onChange={this.onChange}
+          value={email}
+          placeholder="Email"
+          fluid
+        />
+        <Input
+          name="password"
+          onChange={this.onChange}
+          value={password}
+          type="password"
+          placeholder="Password"
+          fluid
+        />
+      </Container>
+    );
+  }
 }
