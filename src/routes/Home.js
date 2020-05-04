2eb017f507c5fb110453663adfad6fac84110cb5
stefan hranek <hranekstefan@gmail.com>
2020-05-04 11:07:29 -1000

    Fixed user mapping

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { gql, graphql } from "react-apollo";
 
-const Home = ({ data: { loading, allUsers } }) =>
-  (loading ? null : allUsers.map((u) => <h1 key={u.id}>{u.email}</h1>));
+const Home = ({ data: { allUsers = [] } }) =>
+  allUsers.map((u) => <h1 key={u.id}>{u.email}</h1>);
 
 const allUsersQuery = gql`
   {
