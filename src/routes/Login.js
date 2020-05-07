96b8b04bd6fdbf884580de52a6efb5eca5f700ee
stefan hranek <hranekstefan@gmail.com>
2020-05-07 12:38:54 -1000

    Fixed typos

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,55 +1,53 @@
-import React from 'react';
-import {extendObservable} from 'mobx';
-import {observer} from 'mobx-react';
+import React from "react";
+import { extendObservable } from "mobx";
+import { observer } from "mobx-react";
 import { Button, Input, Container, Header } from "semantic-ui-react";
 
-export default observer(class Login extends React.Component {
-
+export default observer(
+  class Login extends React.Component {
     constructor(props) {
-        super(props);
+      super(props);
 
-        extendObservable(this, {
-            email: '',
-            password: '',
-        });
-    } 
+      extendObservable(this, {
+        email: "",
+        password: "",
+      });
+    }
 
     onSubmit = () => {
-        const { email, password } = this;
-        console.log("email", email);
-        console.log("password", password);
-    }
+      const { email, password } = this;
+      console.log("email", email);
+      console.log("password", password);
+    };
 
-    onChange = e => {
-        const { name, value } = e.target;
-        this[name] = value;
-    }
+    onChange = (e) => {
+      const { name, value } = e.target;
+      this[name] = value;
+    };
 
     render() {
-        const { email, password } = this;
-        return (
-            <Container text>
-              <Header as="h2">Login</Header>
-              <Input
-                error={!!emailError}
-                name="email"
-                onChange={this.onChange}
-                value={email}
-                placeholder="Email"
-                fluid
-                />
-              <Input
-                error={!!passwordError}
-                name="password"
-                onChange={this.onChange}
-                value={password}
-                type="password"
-                placeholder="Password"
-                fluid
-              />
-              <Button onClick={this.onSubmit}>Submit</Button>
-            </Container>
-          );
-        }
+      const { email, password } = this;
+      return (
+        <Container text>
+          <Header as="h2">Login</Header>
+          <Input
+            name="email"
+            onChange={this.onChange}
+            value={email}
+            placeholder="Email"
+            fluid
+          />
+          <Input
+            name="password"
+            onChange={this.onChange}
+            value={password}
+            type="password"
+            placeholder="Password"
+            fluid
+          />
+          <Button onClick={this.onSubmit}>Submit</Button>
+        </Container>
+      );
     }
-});
+  }
+);
