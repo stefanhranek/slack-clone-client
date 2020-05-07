c8813d8fb21201a3b2f822093ec270e5452d6f1e
stefan hranek <hranekstefan@gmail.com>
2020-05-07 12:32:12 -1000

    Login form set up

---
+++ src/routes/Login.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {extendObservable} from 'mobx';
+import {observer} from 'mobx-react';
+import { Button, Input, Container, Header } from "semantic-ui-react";
+
+export default observer(class Login extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        extendObservable(this, {
+            email: '',
+            password: '',
+        });
+    } 
+
+    render() {
+        const { email, password } = this;
+        return (
+            <Container text>
+              <Header as="h2">Login</Header>
+              <Input
+                error={!!emailError}
+                name="email"
+                onChange={this.onChange}
+                value={email}
+                placeholder="Email"
+                fluid
+                />
+              <Input
+                error={!!passwordError}
+                name="password"
+                onChange={this.onChange}
+                value={password}
+                type="password"
+                placeholder="Password"
+                fluid
+              />
+              <Button onClick={this.onSubmit}>Submit</Button>
+            </Container>
+          );
+        }
+    }
+});
