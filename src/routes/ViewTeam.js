7f3c22f89d5fca09450a84556d5f27bf884cf105
stefan hranek <hranekstefan@gmail.com>
2020-06-15 18:18:48 -1100

    Creating a page for viewing team

---
+++ src/routes/ViewTeam.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default () => (
+  <div className="app-layout">
+    <div className="teams">Teams</div>
+    <div className="channels">Channels</div>
+    <div className="header">Header</div>
+    <div className="messages">
+      <ul className="message-list">
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+    <div className="input">
+      <input type="text" placeholder="CSS Grid Layout Module" />
+    </div>
+  </div>
+);
